10fb44e63b17938f430ec4510202e6c61c2346a1
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-04-27 09:35:58 -0300

    5

---
+++ src/main/java/com/portfolio/demo/Security/Enums/RolNombre.java
@@ -0,0 +1,5 @@
+package com.portfolio.demo.Security.Enums;
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+}
---
+++ src/main/java/com/portfolio/demo/Service/ImpPersonaService.java
@@ -0,0 +1,39 @@
+package com.portfolio.demo.Service;
+
+import com.portfolio.demo.Entity.Persona;
+import com.portfolio.demo.Interface.IPersonaService;
+import com.portfolio.demo.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService {
+
+    @Autowired
+    IPersonaRepository ipersonaRepository;
+    //Dependency injection
+
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public Persona savePersona(Persona persona) {
+        return ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+
+}
