9526b9d573ba875f7a1431150a649c99878c32a9
Alexis Sanz <105322087+AlexisSanz999@users.noreply.github.com>
2023-04-30 14:10:27 -0300

    13

---
+++ src/main/java/com/portfolio/demo/Service/SHyS.java
@@ -0,0 +1,44 @@
+package com.portfolio.demo.Service;
+
+import com.portfolio.demo.Entity.HyS;
+import com.portfolio.demo.Repository.RHyS;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class SHyS {
+    @Autowired
+    RHyS rhys;
+    
+    public List<HyS> list(){
+        return rhys.findAll();
+    }
+    
+    public Optional<HyS> getOne(int id){
+        return rhys.findById(id);
+    }
+    
+    public Optional<HyS> getByNombre(String nombre){
+        return rhys.findByNombre(nombre);
+    }
+    
+    public void save(HyS skill){
+        rhys.save(skill);
+    }
+    
+    public void delete(int id){
+        rhys.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rhys.existsById(id);
+    }
+    
+    public boolean existsByNombre(String nombre){
+        return rhys.existsByNombre(nombre);
+    }
+}
